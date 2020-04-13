5f360609d7edcf20626c2423535b470bdac40f3e
Marashov_Alexander <sanYAMarashov>
2020-04-13 19:38:35 +0300

    Activity adding exercise basic layout

---
+++ app/src/main/java/ru/ok/technopolis/training/personal/activities/exercise/AddingExerciseActivity.kt
@@ -0,0 +1,14 @@
+package ru.ok.technopolis.training.personal.activities.exercise
+
+import android.os.Bundle
+import ru.ok.technopolis.training.personal.activities.BaseActivity
+import ru.ok.technopolis.training.personal.R
+
+class AddingExerciseActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getLayoutId(): Int = R.layout.activity_adding_exercise
+}
