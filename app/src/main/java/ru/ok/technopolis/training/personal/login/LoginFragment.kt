f8ae168b16b2e467f54c5456d949e147a68ed4f7
bogdan.mashchenko <bogdan.mashchenko@corp.mail.ru>
2020-04-14 20:10:51 +0300

    Synthetic imports fix

--- app/src/main/java/ru/ok/technopolis/training/personal/login/LoginFragment.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/login/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_login.*
 import ru.ok.technopolis.training.personal.R
 import ru.ok.technopolis.training.personal.base.BaseFragment
 
@@ -21,14 +22,13 @@ class LoginFragment : BaseFragment() {
     private var loginButton: Button? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = super.onCreateView(inflater, container, savedInstanceState)
-        imageView = root?.findViewById(R.id.person_iv)
-        emailEditText = root?.findViewById(R.id.email_et)
-        passwordEditText = root?.findViewById(R.id.password_et)
-        forgotPasswordTextView = root?.findViewById(R.id.forgot_password_tv)
-        notExistAccTextView = root?.findViewById(R.id.not_exist_acc_tv)
-        loginButton = root?.findViewById(R.id.login_button)
-        return root
+        imageView = person_iv
+        emailEditText = email_et
+        passwordEditText = password_et
+        forgotPasswordTextView = forgot_password_tv
+        notExistAccTextView = not_exist_acc_tv
+        loginButton = login_button
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun getFragmentLayoutId(): Int = R.layout.fragment_login
