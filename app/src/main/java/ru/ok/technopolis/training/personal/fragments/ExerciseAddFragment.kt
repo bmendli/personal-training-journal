13979ee75a5622ee23b55f9696b12be7328f3b26
Marashov_Alexander <sanYAMarashov>
2020-04-15 11:37:27 +0300

    Migration Exercise adding activity to Exercise adding fragment

---
+++ app/src/main/java/ru/ok/technopolis/training/personal/fragments/ExerciseAddFragment.kt
@@ -0,0 +1,9 @@
+package ru.ok.technopolis.training.personal.fragments
+
+import ru.ok.technopolis.training.personal.R
+
+class ExerciseAddFragment : BaseFragment() {
+
+    override fun getFragmentLayoutId(): Int = R.layout.fragment_exercise_add
+
+}
