1d9eeebcc854fdcce2d6036cb8129eb62ba3955f
Marashov_Alexander <sanYAMarashov>
2020-04-17 01:26:01 +0300

    Style Fixes

--- app/src/main/java/ru/ok/technopolis/training/personal/fragments/CalendarFragment.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/fragments/CalendarFragment.kt
@@ -1,11 +1,10 @@
 package ru.ok.technopolis.training.personal.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CalendarView
+import kotlinx.android.synthetic.main.fragment_calendar.view.*
 import ru.ok.technopolis.training.personal.R
 
 class CalendarFragment : BaseFragment() {
@@ -13,13 +12,10 @@ class CalendarFragment : BaseFragment() {
     var calendar: CalendarView? = null
     var buttonAddWorkout: Button? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = super.onCreateView(inflater, container, savedInstanceState)
-
-        calendar = (rootView?.findViewById(R.id.fragment_calendar__calendar)) as CalendarView
-        buttonAddWorkout = (rootView.findViewById(R.id.fragment_calendar__add_workout_button)) as Button
-
-        return rootView
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        calendar = view.calendar
+        buttonAddWorkout = view.add_workout_button
     }
 
     override fun getFragmentLayoutId(): Int = R.layout.fragment_calendar
