25460e3003c425f4e9a1ead848d62b1f2fbadb8a
Artyom Petrov <apetrov@spbtv.com>
2020-04-16 23:16:11 +0300

    Using synthetics imports

--- app/src/main/java/ru/ok/technopolis/training/personal/fragments/ActiveExerciseFragment.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/fragments/ActiveExerciseFragment.kt
@@ -1,14 +1,12 @@
 package ru.ok.technopolis.training.personal.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.frgment_active_exercise.*
+import kotlinx.android.synthetic.main.frgment_active_exercise.view.*
 import ru.ok.technopolis.training.personal.R
 
 class ActiveExerciseFragment : BaseFragment() {
@@ -24,14 +22,14 @@ class ActiveExerciseFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-       goBackView = view.findViewById(R.id.go_back) as ImageView
-        setStarView = view.findViewById(R.id.star_border) as ImageView
-        setBookmarkView = view.findViewById(R.id.bookmark_border) as ImageView
-        parameterGoal1EditText = view.findViewById(R.id.parameter1_goal) as EditText
-        parameterGoal2EditText = view.findViewById(R.id.parameter2_goal) as EditText
-        parameterGoal3EditText = view.findViewById(R.id.parameter3_goal) as EditText
-        doneButton = view.findViewById(R.id.done_button) as Button
-        nextExerciseView = view.findViewById(R.id.next_exercise_name) as TextView
+        goBackView = view.go_back
+        setStarView = view.star_border
+        setBookmarkView = view.bookmark_border
+        parameterGoal1EditText = view.parameter1_goal
+        parameterGoal2EditText = view.parameter2_goal
+        parameterGoal3EditText = view.parameter3_goal
+        doneButton = view.done_button
+        nextExerciseView = view.next_exercise_name
     }
 
     override fun getFragmentLayoutId(): Int = R.layout.frgment_active_exercise;
