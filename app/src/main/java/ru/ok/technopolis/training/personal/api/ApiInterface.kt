9c6fc6f3854d711634045c18478ee94f7979b417
Artyom Petrov <apetrov@spbtv.com>
2020-04-19 23:44:25 +0300

    Api client implementation

---
+++ app/src/main/java/ru/ok/technopolis/training/personal/api/ApiInterface.kt
@@ -0,0 +1,13 @@
+package ru.ok.technopolis.training.personal.api
+
+/**
+ * Your Request-methods should be here
+ */
+interface ApiInterface {
+
+    /**
+     * @GET("path/path...")
+     * fun getUserTrainings(userId: String): Single<TrainingDto>
+     */
+
+}
