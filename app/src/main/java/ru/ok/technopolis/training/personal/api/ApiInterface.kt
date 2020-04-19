d12aec7a043415946cc029b37013b31fbf715d74
akkalomator <42492607+akkalomator@users.noreply.github.com>
2020-04-20 00:20:46 +0300

    Retofit rx suggestion (#17)
    * Api client implementation
    
    * ktlint
    
    Co-authored-by: Artyom Petrov <apetrov@spbtv.com>

---
+++ app/src/main/java/ru/ok/technopolis/training/personal/api/ApiInterface.kt
@@ -0,0 +1,12 @@
+package ru.ok.technopolis.training.personal.api
+
+/**
+ * Your Request-methods should be here
+ */
+interface ApiInterface {
+
+    /**
+     * @GET("path/path...")
+     * fun getUserTrainings(userId: String): Single<TrainingDto>
+     */
+}
--- app/src/main/java/ru/ok/technopolis/training/personal/api/RetrofitApiUtils.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/api/RetrofitApiUtils.kt
@@ -1,23 +1,19 @@
 package ru.ok.technopolis.training.personal.api
 
-import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.ok.technopolis.training.personal.BuildConfig
-import ru.ok.technopolis.training.personal.utils.thread.ThreadUtils
 
 object RetrofitApiUtils {
-
-    val SCHEDULER = Schedulers.from(ThreadUtils.executor)
 
     // todo server url
     private const val TRAINING_JOURNAL_URL = "https://<>.com/"
 
     @JvmStatic
-    fun createApi(): Api {
+    fun createApi(): ApiInterface {
         val okHttpClient = OkHttpClient.Builder()
         if (BuildConfig.DEBUG) {
             val httpLoggingInterceptor = HttpLoggingInterceptor()
@@ -31,6 +27,6 @@ object RetrofitApiUtils {
                 .baseUrl(TRAINING_JOURNAL_URL)
                 .client(okHttpClient.build())
                 .build()
-                .create(Api::class.java)
+                .create(ApiInterface::class.java)
     }
 }
