582eb18895e4c8b024b5575f07190262608cb3fa
bogdan.mashchenko <bogdan.mashchenko@corp.mail.ru>
2020-04-19 19:47:12 +0300

    improved retrofit api

---
+++ app/src/main/java/ru/ok/technopolis/training/personal/api/Api.kt
@@ -0,0 +1,8 @@
+package ru.ok.technopolis.training.personal.api
+
+/**
+ * Your Request-methods should be here
+ */
+interface Api {
+
+}
--- app/src/main/java/ru/ok/technopolis/training/personal/api/RetrofitApiUtils.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/api/RetrofitApiUtils.kt
@@ -1,21 +1,37 @@
 package ru.ok.technopolis.training.personal.api
 
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.ok.technopolis.training.personal.BuildConfig
+import ru.ok.technopolis.training.personal.utils.thread.ThreadUtils
 
 object RetrofitApiUtils {
+
+    val SCHEDULER = Schedulers.from(ThreadUtils.executor)
 
     // todo server url
     private const val TRAINING_JOURNAL_URL = "https://<>.com/"
 
+
     @JvmStatic
-    fun <T> create(clazz: Class<T>): T {
+    fun createApi(): Api {
+        val okHttpClient = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+            okHttpClient.addInterceptor(httpLoggingInterceptor)
+        }
+
         return Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(TRAINING_JOURNAL_URL)
+                .client(okHttpClient.build())
                 .build()
-                .create(clazz)
+                .create(Api::class.java)
     }
 }
