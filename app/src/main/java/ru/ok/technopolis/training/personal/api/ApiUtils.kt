136066b736a7000951e953eabd177238778ada5b
bogdan.mashchenko <bogdan.mashchenko@corp.mail.ru>
2020-05-02 00:45:20 +0300

    ktlint

--- app/src/main/java/ru/ok/technopolis/training/personal/api/ApiUtils.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/api/ApiUtils.kt
@@ -2,8 +2,6 @@ package ru.ok.technopolis.training.personal.api
 
 import okhttp3.Credentials
 
-
 object ApiUtils {
-
     fun encodeEmailAndPasswordToAuthorizationHeader(email: String, password: String): String = Credentials.basic(email, password)
 }
