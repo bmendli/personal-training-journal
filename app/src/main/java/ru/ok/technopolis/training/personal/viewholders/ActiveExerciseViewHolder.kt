6ee9c6cbac649f75b8f6c49342f8e0d290ffde1b
igorl <igorlaryush@yandex.ru>
2020-05-06 19:00:45 +0500

    refactor № 5

--- app/src/main/java/ru/ok/technopolis/training/personal/viewholders/ActiveExerciseViewHolder.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/viewholders/ActiveExerciseViewHolder.kt
@@ -1,7 +1,6 @@
 package ru.ok.technopolis.training.personal.viewholders
 
 import android.view.View
-import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.item_active_exercise_element.view.*
 import ru.ok.technopolis.training.personal.items.ExerciseItem
@@ -11,10 +10,10 @@ class ActiveExerciseViewHolder(
 ) : BaseViewHolder<ExerciseItem>(itemView) {
 
     private var title: TextView = itemView.parameter
-    private var value: EditText = itemView.parameter_goal
+    private var goal: TextView = itemView.parameter_goal
 
     override fun bind(item: ExerciseItem) {
         title.text = item.title
-        value.setText(item.value.toString())
+        goal.text = item.value.toString()
     }
 }
