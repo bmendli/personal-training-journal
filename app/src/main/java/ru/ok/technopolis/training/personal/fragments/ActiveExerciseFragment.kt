59ff3eac6d2a40a136ad4f253bb03214a3359b83
igorl <igorlaryush@yandex.ru>
2020-05-06 18:45:55 +0500

    refactor № 4

--- app/src/main/java/ru/ok/technopolis/training/personal/fragments/ActiveExerciseFragment.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/fragments/ActiveExerciseFragment.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.frgment_active_exercise.view.*
 import ru.ok.technopolis.training.personal.R
-import ru.ok.technopolis.training.personal.items.ActiveExerciseItem
+import ru.ok.technopolis.training.personal.items.ExerciseItem
 import ru.ok.technopolis.training.personal.utils.recycler.adapters.BaseListAdapter
 import ru.ok.technopolis.training.personal.viewholders.ActiveExerciseViewHolder
 
@@ -40,9 +40,9 @@ class ActiveExerciseFragment : BaseFragment() {
         }
 
         val parameters = Observable.just(listOf(
-                ActiveExerciseItem("id1", "Title1", 5, 0, 0),
-                ActiveExerciseItem("id2", "Title2", 5, 1, 1),
-                ActiveExerciseItem("id3", "Title3", 5, 2, 2)
+                ExerciseItem("id1", "Title1", 5, 0, 0),
+                ExerciseItem("id2", "Title2", 5, 1, 1),
+                ExerciseItem("id3", "Title3", 5, 2, 2)
                 )
         )
 
--- app/src/main/java/ru/ok/technopolis/training/personal/viewholders/ActiveExerciseViewHolder.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/viewholders/ActiveExerciseViewHolder.kt
@@ -4,18 +4,16 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.item_active_exercise_element.view.*
-import kotlinx.android.synthetic.main.item_exercise_element.view.*
-import kotlinx.android.synthetic.main.item_workout_element.view.*
-import ru.ok.technopolis.training.personal.items.ActiveExerciseItem
+import ru.ok.technopolis.training.personal.items.ExerciseItem
 
 class ActiveExerciseViewHolder(
     itemView: View
-) : BaseViewHolder<ActiveExerciseItem>(itemView) {
+) : BaseViewHolder<ExerciseItem>(itemView) {
 
     private var title: TextView = itemView.parameter
     private var value: EditText = itemView.parameter_goal
 
-    override fun bind(item: ActiveExerciseItem) {
+    override fun bind(item: ExerciseItem) {
         title.text = item.title
         value.setText(item.value.toString())
     }
