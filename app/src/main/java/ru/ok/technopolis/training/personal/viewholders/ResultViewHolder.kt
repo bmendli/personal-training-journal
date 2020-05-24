9369b5bdac06f11e5cb9e4f7e3b2382f393c1e09
Marashov_Alexander <sanYAMarashov>
2020-05-24 20:57:08 +0300

    Workout results and graphics

---
+++ app/src/main/java/ru/ok/technopolis/training/personal/viewholders/ResultViewHolder.kt
@@ -0,0 +1,31 @@
+package ru.ok.technopolis.training.personal.viewholders
+
+import android.view.View
+import android.widget.TextView
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import kotlinx.android.synthetic.main.item_result.view.*
+import ru.ok.technopolis.training.personal.db.model.ParameterResultModel
+
+class ResultViewHolder(
+    itemView: View
+) : BaseViewHolder<ParameterResultModel>(itemView) {
+
+    private var graphTitle: TextView = itemView.graph_title
+    private var graph: GraphView = itemView.graph_layout
+
+    override fun bind(item: ParameterResultModel) {
+        val unitId = item.parameter.measureUnitId
+        val unitText = item.measureUnitChoices[unitId.toInt() - 1]
+        graphTitle.text = String.format("%s (%s)", item.parameter.name, unitText)
+        var x = 1.0
+        val data = item.resultsParameterList.map {
+            println(it.value)
+            x += 1.0
+            DataPoint(x, it.value.toDouble())
+        }.toTypedArray()
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(data)
+        graph.addSeries(series)
+    }
+}
